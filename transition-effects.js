2893737c20a19204f1c86d854c78ae493f2f6f3b
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-20 17:54:37 +0530

    adding files for my resume website

---
+++ transition-effects.js
@@ -0,0 +1,7 @@
+function toggleOptions() {
+    document.querySelector('.option-1').style.animation = "smooth-show-option-1 1s ease-in 1s forwards";
+    document.querySelector('.option-2').style.animation = "smooth-show-option-2 1s ease-in 1s forwards";
+    document.querySelector('.option-3').style.animation = "smooth-show-option-3 1s ease-in 1s forwards";
+    document.querySelector('.option-4').style.animation = "smooth-show-option-4 1s ease-in 1s forwards";
+    // write code here to clear the text Click here to know more
+}
